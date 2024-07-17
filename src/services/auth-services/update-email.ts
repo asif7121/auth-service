bf4a9062ad80a52735c5fcc25403ead3f58f9eff
Asif <ansariasif449@gmail.com>
2024-07-17 16:50:45 +0530

    fixed update validation for email and phone
    Signed-off-by: Asif <ansariasif449@gmail.com>

--- src/services/auth-services/update-email.ts
+++ src/services/auth-services/update-email.ts
@@ -6,13 +6,15 @@ import { send_email } from "@services/two-factor-auth"
 
 
 export const updateEmail = async (userId:string, newEmail:string) => {
-	const user = await Auth.findByIdAndUpdate(userId, {
-		email: newEmail
-	},{new:true})
+	const user = await Auth.findById(userId)
 	if (!user) throw new Error('User not found.')
+	if (user.email === newEmail) {
+		throw new Error('Existing email and entered email cannot be same')
+	}
+	user.email = newEmail
 	const otp = generate_random_number(6).toString()
-	await send_email(user.email, otp)
 	user.otp = otp
 	await user.save()
+	await send_email(user.email, otp)
 	return user
 }
